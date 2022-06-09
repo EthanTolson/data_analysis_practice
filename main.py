1c7f75b1fe7e5711b5ee55ca209cd7daa62fd5da
EthanTolson <ethantolson1@gmail.com>
2022-06-08 18:51:25 -0600

    Added data set and started learning pandas

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
+from os import path
+FILEPATH = f"{path.dirname(path.abspath(__file__))}"
 
 def main():
-    pass
+    dataframe = pd.read_csv(FILEPATH+"\\Tesla.csv")
+    print(dataframe.head(2))
+    print(dataframe.tail(2))
+    print(dataframe.mean(0))
 
 if __name__ =="__main__":
     main()
