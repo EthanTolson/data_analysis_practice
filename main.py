42b2af7a91db397f80cbd95a2c70cf5712325bf0
EthanTolson <ethantolson1@gmail.com>
2022-06-13 14:00:20 -0600

    Now shows a graph of open prices

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 from os import path
 FILEPATH = f"{path.dirname(path.abspath(__file__))}"
 
@@ -10,6 +11,21 @@ def main():
     print(dataframe.tail(2))
     print("------------------------------")
     print(dataframe.mean(0, numeric_only=True, skipna=True))
+    print(dataframe.describe())
+    print()
+    #print(dataframe["Open"].to_numpy())
+    for column in dataframe:
+        print(column)
+
+    # Graph of open prices over time
+    date = dataframe["Date"]
+    open_price = dataframe["Open"]
+
+    plt.bar(date, open_price)
+    plt.title("Open Prices over Time")
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.show()
 
 if __name__ =="__main__":
     main()
